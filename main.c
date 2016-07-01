ea0fba51c8fe11fba81cbc274285751f6c1fa5e4
Jacó Macena de Araújo <jacog3@gmail.com>
2016-07-01 13:43:46 -0300

    Update main.c
    adicionado biblioteca para acentuação.

--- main.c
+++ main.c
@@ -1,42 +1,44 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h> // Biblioteca para acentuação
 
 int main()
 {
+    setlocale(LC_ALL,""); // função para acentos
     int num, tent = 1, seu_num, again = 1;
 
     do{
         num = rand()%99+1;
-        printf("\nAdivinhe o numero que eu estou pensando: ");
+        printf("\nAdivinhe o número que eu estou pensando: ");
         scanf("%d", &seu_num);
 
         while(seu_num != num){
             if(tent < 5){
-                // Verifica��o do numero
+                // Verificação do numero
                 if(seu_num < num){
-                    printf("\nO meu numero e maior que %d\n",seu_num);
+                    printf("\nO meu número é maior que %d\n",seu_num);
                 }else{
-                    printf("\nO meu numero e menor que %d\n",seu_num);
+                    printf("\nO meu número é menor que %d\n",seu_num);
                 }
                 tent++;
 
                 printf("\nTente novamente: ");
                 scanf("%d", &seu_num);
             }else{
-                printf("Voce nao conseguiu acertar!\n");
+                printf("Você não conseguiu acertar!\n");
                 printf("Deseja jogar novamente?\n");
-                printf("\n1 - Sim\n2- Nao\n");
+                printf("\n1 - Sim\n2- Não\n");
                 scanf("%d", &again);
                 if(again==2) exit(1);
             }
         }
 
-        printf("Parabens, voce acertou!\n");
-        printf("O meu numero foi: %d\n\n", num);
+        printf("Parabéns, você acertou!\n");
+        printf("O meu número foi: %d\n\n", num);
 
 
         printf("Deseja jogar novamente?\n");
-        printf("\n1 - Sim\n2- Nao\n");
+        printf("\n1 - Sim\n2- Não\n");
         scanf("%d", &again);
         free(&num);
 
